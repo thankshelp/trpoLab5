de059a478b558e2852ebcf82a125d25ed25f8ae2
LittelHell13 <hottelmich@gmail.com>
2019-03-19 13:14:05 +0700

    start

---
+++ Lab5/Lab5/functions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class functions
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double con(double a)
+        {
+          return a * 2.54;
+            
+        }
+
+        public bool chet(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int max(int[] m)
+        {
+            int max = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                if (m[i] > max)
+                    max = m[i];
+            }
+            return max;
+        }
+
+        public int ost(int a, int b)
+        {
+            return a % b;
+        }
+
+    }
+}
---
+++ Lab5/Lab5/test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lab5
+{
+    [TestFixture]
+    class test
+    {
+        [TestCase]
+        public void Add()
+        {
+            functions f = new functions();
+            Assert.AreEqual(33, f.add(3, 30));
+        }
+
+        [TestCase]
+        public void Sub()
+        {
+            functions f = new functions();
+            Assert.AreEqual(33, f.sub(3, 30));
+        }
+
+        [TestCase]
+        public void Con()
+        {
+            functions f = new functions();
+            Assert.AreEqual(15.24, f.con(6));
+        }
+
+        [TestCase]
+        public void Chet()
+        {
+            functions f = new functions();
+            Assert.AreEqual(true, f.chet(12));
+        }
+
+        [TestCase]
+        public void Max()
+        {
+            int[] m = { 0, 4, 10, 3, 7 };
+
+            functions f = new functions();
+            Assert.AreEqual(10, f.max(m));
+        }
+
+        [TestCase]
+        public void Ost()
+        {
+            functions f = new functions();
+            Assert.AreEqual(3, f.ost(7, 4));
+        }
+    }
+}
